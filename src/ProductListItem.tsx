60a07721bd23ce4fddca3e3b124f0c73bbc1becc
crisner <christie.reni@gmail.com>
2020-05-23 19:02:37 +0530

    Add icon on hover to expand product information

--- src/ProductListItem.tsx
+++ src/ProductListItem.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-regular-svg-icons';
 import ProductDescriptionModal from './ProductDescriptionModal';
 
 interface ProductListItemType {
@@ -13,14 +15,26 @@ interface ProductListItemType {
 const ProductListItem = (props: ProductListItemType): JSX.Element => {
   const [quantity, setQuantity] = useState(1);
   const [modal, setModal] = useState(false);
+  const productImg = useRef(null);
+  const [width, setWidth] = useState(0);
   const dataHandler = (quantity: number) => setQuantity(quantity);
   const closeModal = () => setModal(false);
 
   return (
     <>
-      <div className="product-list-item">
-        <img className="product-image" src={props.image}
-        alt="Image of Product name" onClick={() => setModal(true)} />
+      <div className="product-list-item" ref={productImg} 
+      onLoad={() => setWidth(productImg.current.clientWidth)} >
+        <div className="product-image-container" 
+        onClick={() => setModal(true)}
+        style={{width:`${width}px`, height: `${width}px`}}>
+          <div className="view-product-info" 
+          style={{width:`${width}px`, height: `${width}px`}}>
+            <FontAwesomeIcon className="view-icon" icon={faEye} />
+          </div>
+          <img className="product-image" src={props.image}
+          alt="Image of Product name" />
+        </div>
+        
         <div className="product-footer">
           <h3 className="product-title">{props.name}</h3>
           <div className="product-details">
