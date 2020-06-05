b074ca3b905ca6af2a6b238212e0823948453f54
crisner <christie.reni@gmail.com>
2020-06-05 19:12:53 +0530

    Fix title width bug

--- src/CollectionsItem.tsx
+++ src/CollectionsItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
 interface CollectionsItemProps {
   img: string,
@@ -7,27 +7,22 @@ interface CollectionsItemProps {
   width?: string
 }
 
-// const getImage = getImage
-
 const CollectionsItem = (props: CollectionsItemProps): JSX.Element => {
   
-  let imageEl = React.useRef(null);
-
-  document.addEventListener('DOMContentLoaded', (event) => {
-    let text = document.querySelectorAll<HTMLElement>('.collections-item .title');
-    text.forEach(el => el.style.width = (imageEl.current.width/2) + 'px')
-  })
-
+  const collectionContainer = useRef(null);
+  const [width, setWidth] = useState(0);
   
   return (
-  <div className="collections-item">
-    <img className="image" ref={imageEl}
-     src={props.img} 
-     alt={props.alt} 
-     width={props.width} 
-      />
-    <h4 className="title">{props.title}</h4>
-  </div>
-)}
+    <div className="collections-item" ref={collectionContainer}
+    onLoad={() => setWidth(collectionContainer.current.clientWidth)}>
+      <img className="image"
+      src={props.img} 
+      alt={props.alt} 
+      width={props.width} 
+        />
+      <h4 className="title" style={{'width': `${(width/2)}px`}}>{props.title}</h4>
+    </div>
+  )
+}
 
 export default CollectionsItem;
