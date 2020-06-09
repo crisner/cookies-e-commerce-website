88a47d9f254ece9084826d42d9168ec5cf26e784
crisner <christie.reni@gmail.com>
2020-06-09 15:11:16 +0530

    Update image width and remove commented out button

--- src/BoxItem.tsx
+++ src/BoxItem.tsx
@@ -8,12 +8,11 @@ const BoxItem = ({selectedPiece, removePiece}): JSX.Element => (
   <div className="box-item">
     <p className="selected-quantity">{selectedPiece.quantity} x</p>
     <div className="preview-img" style={{contain: 'content'}}>
-      <img src={selectedPiece.image} alt={`Image of ${selectedPiece.name} cookies`} style={{width: '4rem'}} />
+      <img src={selectedPiece.image} alt={`Image of ${selectedPiece.name} cookies`} style={{width: '100%'}} />
     </div>
     <p className="product-name">{selectedPiece.name}</p>
     <p className="product-cost">Rs. {selectedPiece.price}</p>
     <FontAwesomeIcon className="product-remove" icon={faTrashAlt} onClick={() => removePiece(selectedPiece.id)} />
-    {/* <button className="btn-add product-remove">X</button> */}
   </div>
 )
 
