578632e01fc9ef2e590ec11d776f89122c51eda6
crisner <christie.reni@gmail.com>
2020-06-09 19:11:45 +0530

    Add selector to fetch single selected item

--- src/redux/selectors.tsx
+++ src/redux/selectors.tsx
@@ -1,1 +1,3 @@
 export const getSelectedPieces = store => store.boxBuilder.selectedPieces;
+
+export const getSelectedPiece = (store, id) => store.boxBuilder.selectedPieces.filter(piece => piece.id === id);
