186705dd01afb58d13f562eeb721fa6e06537a47
crisner <christie.reni@gmail.com>
2020-06-13 22:54:39 +0530

    Display custom boxes and contents when added to cart

--- src/cart/Cart.tsx
+++ src/cart/Cart.tsx
@@ -3,40 +3,59 @@ import { connect } from 'react-redux';
 import BoxItem from '../buildABox/BoxItem';
 import { AppState } from '../store';
 import { CartState } from '../store/cart/types';
-import { getCartItems } from '../store/cart/selectors';
+import { getBoxesInCart, getCustomBoxesInCart } from '../store/cart/selectors';
 
 type Props = CartState;
 
-const Cart = ({itemsInCart}: Props): JSX.Element => (
-  <div className="cart">
-    <h1>Cart</h1>
-    <h4>Your cart is empty</h4>
+const Cart = ({itemsInCart}: Props): JSX.Element => {
 
-    {itemsInCart && itemsInCart.length ? 
-    itemsInCart.map((item, index) => <BoxItem key={item.id + index} selectedItem={item} />) : null}
-   
-    <div className="subtotal">
-      <div className="subtotal-row">
-        <span>Subtotal</span>
-        <span>Rs'0.00'</span>
+  const customBoxes = Object.entries(itemsInCart.customBoxes);
+  
+  return (
+    <div className="cart">
+      <h1>Cart</h1>
+      <h4>Your cart is empty</h4>
+
+      {itemsInCart.boxes && itemsInCart.boxes.length ? 
+      itemsInCart.boxes.map((item, index) => <BoxItem key={item.id + index} selectedItem={item} />) : null}
+
+      {customBoxes && customBoxes.length ? 
+      customBoxes.map(([key, value], index) => {
+        return (
+          <div key={key}>
+            <p style={{fontSize: '1.3rem'}}>Custom Box {index + 1}</p>
+            {value.map((item, index) => <BoxItem key={item.id + index} selectedItem={item} />)}
+          </div>);
+      }): null}
+      
+      <div className="subtotal">
+        <div className="subtotal-row">
+          <span>Subtotal</span>
+          <span>Rs'0.00'</span>
+        </div>
+        <div className="subtotal-row">
+          <span>Shipping</span>
+          <span>Rs'100.00'</span>
+        </div>
       </div>
-      <div className="subtotal-row">
-        <span>Shipping</span>
-        <span>Rs'100.00'</span>
+      <div className="total">
+          <p>Total</p>
+          <p>Rs'0.00'</p>
       </div>
+      <button className="btn-add">Checkout</button>
     </div>
-    <div className="total">
-        <p>Total</p>
-        <p>Rs'0.00'</p>
-    </div>
-    <button className="btn-add">Checkout</button>
-  </div>
-)
+  )
+}
 
 const mapStateToProps = (state: AppState) => {
-  const itemsInCart = getCartItems(state);
+  const boxes = getBoxesInCart(state);
+  const customBoxes = getCustomBoxesInCart(state);
   return {
-    itemsInCart
+    itemsInCart: {
+      ...state.cart.itemsInCart,
+      boxes,
+      customBoxes
+    }
   }
 }
 
