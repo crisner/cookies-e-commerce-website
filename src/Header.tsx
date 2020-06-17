52b4486143942ab02de59f3e6c6888409d052858
crisner <christie.reni@gmail.com>
2020-06-17 21:06:02 +0530

    Add link to Contact component in menu

--- src/Header.tsx
+++ src/Header.tsx
@@ -19,7 +19,7 @@ const Header = (): JSX.Element => (
       <li><Link to="/">Home</Link></li>
       <li><Link to="/cookies">Cookies</Link></li>
       <li><Link to="/build-a-box">Build your own box</Link></li>
-      {/* <li><Link to="/">Contact</Link></li> */}
+      <li><Link to="/contact">Contact</Link></li>
     </ul>
   </header>
 )
