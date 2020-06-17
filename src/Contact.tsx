77e22c83088ddd243aef9a1900c261a1d845dde9
crisner <christie.reni@gmail.com>
2020-06-17 21:04:09 +0530

    Create Contact component

---
+++ src/Contact.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Contact = (): JSX.Element => (
+  <div className="contact">
+    <div className="support">
+      <h2>Help and Support</h2>
+      <div>
+        <p>0-xxx-xxx-xxxx</p>
+        <p>support@cookiesinc.com</p>
+      </div>
+      
+    </div>
+    <div className="bulk-orders">
+      <h2>Bulk orders</h2>
+      <p>bulkorders@cookiesinc.com</p>
+    </div>
+  </div>
+)
+
+export default Contact;
