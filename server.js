c9397d042fb3abfba9aad5e2330c291c6c663b8b
crisner <christie.reni@gmail.com>
2020-06-22 20:52:03 +0530

    Fix deprecation warning

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const db = require('./config/keys').mongoURI;
 
 // Connect to mongo
 mongoose
-  .connect(db, {useNewUrlParser: true})
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log('mongo connected'))
   .catch(err => console.log(err));
 
