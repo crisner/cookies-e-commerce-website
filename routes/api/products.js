3d8fdf81cae6e122b8d126463c138ecb8de5e40b
crisner <christie.reni@gmail.com>
2020-06-22 20:52:35 +0530

    Add post and delete routes for product api

--- routes/api/products.js
+++ routes/api/products.js
@@ -10,4 +10,31 @@ router.get('/', (req, res) => {
     .then(products => res.json(products))
 })
 
+// POST a product to api/products
+router.post('/', (req, res) => {
+  const newProduct = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    price: req.body.price,
+    boxPrice: req.body.boxPrice,
+    description: req.body.description
+  });
+
+  newProduct.save().then(item => res.send(item));
+})
+
+// DELETE a product
+router.delete('/:id', (req, res) => {
+  Product.findByIdAndDelete(req.params.id)
+    .then(product => {
+      if(!product) {
+        res.status(404);
+        res.send('not found')
+      } else {
+        res.send('success');
+      }
+    })
+    .catch(err => res.status(500))
+})
+
 module.exports = router;
