17d65418e76a92ffdb3f86402eddae4fd8fbd381
crisner <christie.reni@gmail.com>
2020-07-05 19:59:04 +0530

    Add unique property to email

--- models/User.js
+++ models/User.js
@@ -15,6 +15,7 @@ const UserSchema = new Schema ({
     type: String,
     required: true,
     trim: true,
+    unique: true,
     lowercase: true,
     validate(value) {
       if(!validator.isEmail(value)) {
