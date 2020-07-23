c92f0fd48f65e795ee7d5db220a850ca95d54043
crisner <christie.reni@gmail.com>
2020-07-23 23:18:22 +0530

    Update isAuth to be used with passport google

--- services/authMiddleware.js
+++ services/authMiddleware.js
@@ -1,38 +1,49 @@
 const passport = require('passport');
 
 const isAuth = (req, res, next) => {
-  passport.authenticate('jwt', function(err, user, info) {
-    if (err) { 
-      return next(err);
-    }
-    
-    try {
-      if (!user) {
-        res.status(404);
-        return res.redirect('/login');
-      }
-
-      // Check if user has a valid token
-      if (user) {
-        const token = user.tokens.filter(token => token.token === req.cookies.jwt)
-        if (token && token.length === 0) {
-          return res.status(401).redirect('/login');
+  try {
+    if(req.isAuthenticated()) {
+      console.log(req.user);
+      next();
+    } 
+    else if(req.cookies && req.cookies.jwt) {
+      passport.authenticate('jwt', function(err, user, info) {
+        if (err) { 
+          return next(err);
         }
-      }
-
-      req.logIn(user, function(err) {
-        if (err) {
-          return next(err); 
+        
+        if (!user) {
+          res.status(404);
+          return res.redirect('/login');
         }
-        req.token = req.cookies.jwt;
-        req.user = user;
-        next();
-      });
+  
+        // Check if user has a valid token
+        if (user) {
+          const token = user.tokens.filter(token => token.token === req.cookies.jwt)
+          if (token && token.length === 0) {
+            return res.status(401).redirect('/login');
+          }
+        }
+  
+        req.logIn(user, function(err) {
+          if (err) {
+            return next(err); 
+          }
+          req.token = req.cookies.jwt;
+          req.user = user;
+          next();
+        });
 
-    } catch (err) {
-      res.status(401).send({ error: 'Please sign in to proceed.' });
+      })(req, res, next);
+      
+    } else {
+      next();
     }
-  })(req, res, next);
+  } catch(err) {
+    console.log(err)
+    res.status(401).send({ error: 'Please sign in to proceed.' });
+  }
+
 };
 
 const isAdmin = (req, res, next) => {
