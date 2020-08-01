62907b768516cbf6bf9a3d1109fceed9a821d170
crisner <christie.reni@gmail.com>
2020-08-01 21:07:49 +0530

    Replace promises with async

--- routes/api/products.js
+++ routes/api/products.js
@@ -6,36 +6,49 @@ const {isAuth, isAdmin} = require('../../services/authMiddleware');
 const Product = require('../../models/Product');
 
 // GET all products
-router.get('/', (req, res) => {
-  Product.find()
-    .then(products => res.json(products))
+router.get('/', isAuth, async(req, res) => {
+  try {
+    const products = await Product.find({});
+    if(!products) {
+      return res.status(400).send('No products')
+    }
+    res.send(products);
+  } catch(err) {
+    res.status(500).send({ error: err })
+  }
 })
 
 // POST a product to api/products
-router.post('/', isAuth, isAdmin, (req, res) => {
-  const newProduct = new Product({
-    name: req.body.name,
-    image: req.body.image,
-    price: req.body.price,
-    boxPrice: req.body.boxPrice,
-    description: req.body.description
-  });
-
-  newProduct.save().then(item => res.send(item));
+router.post('/', isAuth, isAdmin, async(req, res) => {
+  try {
+    const newProduct = new Product({
+      name: req.body.name,
+      image: req.body.image,
+      price: req.body.price,
+      boxPrice: req.body.boxPrice,
+      description: req.body.description
+    });
+  
+    await newProduct.save()
+    res.send(newProduct)
+  } catch(err) {
+    res.status(500).send({ error: err })
+  }
 })
 
 // DELETE a product
-router.delete('/:id', isAuth, isAdmin, (req, res) => {
-  Product.findByIdAndDelete(req.params.id)
-    .then(product => {
-      if(!product) {
-        res.status(404);
-        res.send('not found')
-      } else {
-        res.send('success');
-      }
-    })
-    .catch(err => res.status(500))
+router.delete('/:id', isAuth, isAdmin, async(req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if(!product) {
+      res.status(404);
+      res.send('not found')
+    }
+    res.send('deleted');
+  } catch(err) {
+    res.status(500).send({ error: err });
+  }
 })
+
 
 module.exports = router;
