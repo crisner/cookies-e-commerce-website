0e18d74288ccc47e52850233726284336edb8c79
crisner <christie.reni@gmail.com>
2020-08-01 21:08:27 +0530

    Add get and patch requests for a single product

--- routes/api/products.js
+++ routes/api/products.js
@@ -18,6 +18,20 @@ router.get('/', isAuth, async(req, res) => {
   }
 })
 
+// GET single product
+router.get('/:id', isAuth, async(req, res) => {
+  try {
+    const product = await findByIdAndDelete(req.params.id);
+    if(!product) {
+      res.status(404);
+      res.send('not found')
+    }
+    res.status(200).send('success');
+  } catch(err) {
+    res.status(500).send({ error: err });
+  } 
+})
+
 // POST a product to api/products
 router.post('/', isAuth, isAdmin, async(req, res) => {
   try {
@@ -33,6 +47,28 @@ router.post('/', isAuth, isAdmin, async(req, res) => {
     res.send(newProduct)
   } catch(err) {
     res.status(500).send({ error: err })
+  }
+})
+
+// Edit product information
+router.patch('/:id', isAuth, isAdmin, async(req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'image', 'imageLarge', 'price', 'boxPrice', 'description'];
+  const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+  if(!isValidOperation) {
+    return res.status(400).send({error: 'Update invalid'})
+  }
+  try {
+    const product = await Product.findById(req.params.id);
+    if(!product) {
+      return res.status(404).send('Product not found');
+    }
+    console.log(req.body)
+    updates.forEach(update => product[update] = req.body[update]);
+    await product.save();
+    res.send(product);
+  } catch(err) {
+    res.status(400).send(err)
   }
 })
 
