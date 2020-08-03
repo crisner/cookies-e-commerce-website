fa103d6b8084abdb2c52fa5717d6f0fcef92d592
crisner <christie.reni@gmail.com>
2020-08-03 20:14:22 +0530

    Add model for orders

---
+++ models/Order.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const OrderSchema = new Schema({
+  number: {
+    type: String,
+    required: true
+  },
+  items: {
+    type: Array,
+    required: true
+  },
+  shippingCost: {
+    type: Number
+  },
+  totalCost: {
+    type: Number,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['placed', 'confirmed', 'dispatched', 'delivered'],
+    required: true
+  },
+  paid: {
+    type: Boolean,
+    required: true
+  },
+  createdOn: {
+    type: Date,
+    required: true
+  },
+  updatedOn: Date
+})
+
+// Add date
+OrderSchema.pre('save', async function (next) {
+  const order = this;
+  if(!order.createdOn) {
+    order.createdOn = new Date().getTime();
+  } else if(order.isModified('items')) {
+    order.updatedOn = new Date().getTime();
+  }
+  next();
+})
+
+module.exports = Order = mongoose.model('orders', OrderSchema);
