b837063ccd423468c02495115f27e56858509af1
crisner <christie.reni@gmail.com>
2020-08-23 15:19:24 +0530

    Pass in logout dispatch call

--- client/src/Dropdown.tsx
+++ client/src/Dropdown.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-const Dropdown = (props): JSX.Element => (
+const Dropdown = (props: any): JSX.Element => (
   <div className="dropdown-menu" id={props.id}>
     {Object.entries(props.data)
     .map(([text, route], index) => (
-      <li key={`${props.id}-${index}`} 
-      className="dropdown-item" 
-      onClick={() => (
-        text === 'Logout' ? 
-        window.location.reload(true) : null
-      )}>
-        <Link to={route}>{text}</Link>
-      </li>)
+      text === 'Logout' ? (
+        <li key={`${props.id}-${index}`} 
+        className="dropdown-item" 
+        onClick={ props.logOut }
+        >
+          <a>{text}</a>
+        </li>
+      ) : (
+        <li key={`${props.id}-${index}`} 
+        className="dropdown-item" 
+        >
+          <Link to={route}>{text}</Link>
+        </li>
+      )
+    ) 
     )}
   </div>
 )
