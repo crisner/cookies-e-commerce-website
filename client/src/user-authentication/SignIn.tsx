323ea51926a593b1ff1145b4e7942efa2a1942c6
crisner <christie.reni@gmail.com>
2020-08-23 15:26:07 +0530

    Connect redux auth state to signin

--- client/src/user-authentication/SignIn.tsx
+++ client/src/user-authentication/SignIn.tsx
@@ -1,33 +1,73 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { connect, ConnectedProps } from 'react-redux';
+import { AppState } from '../store';
+import { signIn } from '../store/auth/actions';
+import { getErrorMessage } from '../store/auth/selectors';
+import { Link, useHistory } from 'react-router-dom';
 
-const SignIn = () => (
-  <div className="sign-in">
-    <h2>Sign In</h2>
-    <div className="form-container">
-      <form className="form" action="" method="post">
-        <label htmlFor="email">
-          <h4>Email</h4>
-          <input className="input-field" type="email" name="email" id="email" />
-        </label>
-        <label htmlFor="password">
-          <h4>Password</h4>
-          <input className="input-field" type="password" name="password" id="password" />
-        </label>
-        <button className="cta" type="submit">Sign In</button>
-      </form>
-      <div className="options">
-        <p>Forgot your password?</p>
-        <p><Link to="/signup">New user? Sign Up</Link></p>
-      </div>
-      <div className="alternate-sign-in">
-        <p>or sign in with</p>
-        <button className="cta" onClick={() => {
-          window.location.pathname = "/auth/google";
-        }}>Google</button>
+const mapStateToProps = (state: AppState) => {
+  const signInError = getErrorMessage(state);
+  return {
+    error: {signInError}
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    signIn: (values, history) => dispatch(signIn(values, history))
+  }
+}
+
+const connector = connect(mapStateToProps, mapDispatchToProps)
+
+type PropsFromRedux = ConnectedProps<typeof connector>
+
+
+const SignIn = ({signIn, error}:PropsFromRedux) => {
+  const history = useHistory();
+  const [userCredentials, setValues] = useState({
+    email: '',
+    password: ''
+   })
+   const handleSubmit = e => {
+    e.preventDefault();
+    signIn(userCredentials, history)
+  }
+  return (
+    <div className="sign-in">
+      <h2>Sign In</h2>
+      <div className="form-container">
+        <form className="form" onSubmit={handleSubmit}>
+          <label htmlFor="email">
+            <h4>Email</h4>
+            <input className="input-field" 
+            type="email" name="email" id="email"
+            onChange={ e => setValues( {...userCredentials, email: e.target.value})}
+            value={userCredentials.email} />
+          </label>
+          <label htmlFor="password">
+            <h4>Password</h4>
+            <input className="input-field" 
+            type="password" name="password" id="password"
+            onChange={ e => setValues( {...userCredentials, password: e.target.value})}
+            value={userCredentials.password} />
+          </label>
+          <button className="cta" type="submit">Sign In</button>
+          <p>{error && error.signInError}</p>
+        </form>
+        <div className="options">
+          <p>Forgot your password?</p>
+          <p><Link to="/signup">New user? Sign Up</Link></p>
+        </div>
+        <div className="alternate-sign-in">
+          <p>or sign in with</p>
+          <button className="cta" onClick={() => {
+            window.location.pathname = "/auth/google";
+          }}>Google</button>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
-export default SignIn;
+export default connector(SignIn);
