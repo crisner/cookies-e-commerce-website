f179f7251651720b693eaac07553993ffcfcda82
crisner <christie.reni@gmail.com>
2020-08-26 21:48:57 +0530

    Update webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,12 +1,58 @@
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// module.exports = {
+//   entry: './src/index.tsx',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'public', 'dist'),
+//     publicPath: '/'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.tsx?$/,
+//         use: 'ts-loader',
+//         exclude: /node_modules/
+//       }
+//     ]
+//   },
+//   resolve: {
+//     extensions: ['.tsx', '.ts', '.js']
+//   },
+//   devtool: 'inline-source-map',
+//   watch: true,
+//   devServer: {
+//     contentBase: path.join(__dirname, 'public'),
+//     historyApiFallback: true,
+//     publicPath: '/dist',
+//     proxy: [{
+//       context: ['/auth/*', '/api/*'],
+//       target: 'http://localhost:5000',
+//     }]
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: 'public/index.html'
+//     })
+//   ]
+// }
+
+
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public', 'dist'),
     publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
   },
   module: {
     rules: [
@@ -14,24 +60,80 @@ module.exports = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/
+      },
+      {
+        test:/\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/react'
+            ],
+            plugins: [
+              '@babel/plugin-transform-regenerator', 
+              'transform-class-properties'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true
+            }
+          },
+          'postcss-loader'
+        ],
+        include: /\.module\.css$/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        exclude: /\.module\.css$/
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        include: path.join(__dirname, '/src/images'),
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: path.join(__dirname, '/public/dist'),
+            publicPath: '/',
+            // emitFile: false
+          },
+        },
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            bypassOnDebug: true,
+          }
+        }]
       }
     ]
   },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js']
-  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html')
+    }),
+    new MiniCssExtractPlugin({filename: 'main.css',})
+  ],
   devtool: 'inline-source-map',
   watch: true,
   devServer: {
+    contentBase: path.resolve(__dirname, 'public', 'dist'),
+    publicPath: '/',
     historyApiFallback: true,
     proxy: [{
-      context: ['/auth', '/api'],
+      context: ['/auth/*', '/api/*'],
       target: 'http://localhost:5000',
     }]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'public/index.html'
-    })
-  ]
+  }
 }
